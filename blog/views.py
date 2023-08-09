eabb601693e401bf37e904f59d22ae967155af63
MURILO <cg301617x@LAB204M16.IFSP>
2023-08-09 11:04:52 -0300

    09/08

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+# Incluir a classe httpresponse.
 from django.http import HttpResponse
 
+# Define uma function view chamada index.
 def index(request):
-    #return HttpResponse('Olá Django - index')#
-    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
+#   return HttpResponse('Ola Django - index')
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})# novo retorno
 
+# Define uma function view chamada ola.
 def ola(request):
-    return HttpResponse('Olá Django')
+    return render(request, 'home.html')
