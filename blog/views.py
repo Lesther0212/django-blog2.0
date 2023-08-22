bde2d87afc6e5c6d0606c693df9dc7ec2d454286
Lesther0212 <murilodrumondlesther@gmail.com>
2023-08-22 09:06:14 -0300

    Configurado banco MySQL

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,12 @@ def index(request):
 # Define uma function view chamada ola.
 def ola(request):
     return render(request, 'home.html')
+
+from blog.models import Post # Acrescentar
+def ola(request): # Modificar
+    # return HttpResponse('Olá django')
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
+
+
